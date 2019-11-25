cabcfe5c4a0b1a91fbee524dc4927e11faefec71
Jonathan Parnell II <JonathanParnell2@Jonathans-MacBook-Air.local>
2019-11-24 23:16:59 -0800

    First commit for deliverable two

---
+++ deliverabletwo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string message;
+        Console.WriteLine("what is your message?");
+        message = Console.ReadLine();
+        Console.WriteLine("{0}", message.ToUpper());
+        Console.ReadLine();
+
+    }
+}
